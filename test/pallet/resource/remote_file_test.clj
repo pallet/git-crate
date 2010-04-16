pallet/pallet

commit 3bb22c7792005fa711c66c4b77d31fe18cb0ec69

fixed remote-file if statement, pushed *file-transfers* into produce-phases, added tests

src/pallet/core.clj
@@ -24,7 +24,7 @@ tag as a configuration target.
   (:use
    [pallet.utils
     :only [remote-sudo make-user *admin-user* default-public-key-path
-           as-string *file-transfers*]]
+           as-string]]
    [pallet.resource
     :only [produce-phases defphases]]
    [pallet.compute
@@ -206,11 +206,10 @@ script that is run with root privileges immediatly after first boot."
         options (if port [:port port] [])]
     (if node-info
       (doseq [phase phases]
-        (binding [*file-transfers* {}]
-          (when-let [script (produce-phases [phase] (node-info :tag) (node-info :image)
-                                            (node-info :phases))]
-            (info script)
-            (apply execute-script script node user options))))
+        (when-let [script (produce-phases [phase] (node-info :tag) (node-info :image)
+                                          (node-info :phases))]
+          (info script)
+          (apply execute-script script node user options)))
       (error (str "Could not find node type for node " (tag node))))))
 
 (defn apply-phases

src/pallet/resource.clj
@@ -4,7 +4,7 @@
   (:use [pallet.target
          :only [with-target-template with-target-tag
                 *target-tag* *target-template*]]
-        [pallet.utils :only [cmd-join]]
+        [pallet.utils :only [cmd-join *file-transfers*]]
         [pallet.stevedore :only [script]]
         [clojure.contrib.def :only [defvar name-with-attributes]]
         clojure.contrib.logging))
@@ -159,13 +159,14 @@ args is the argument signature for the resource, and must end with a variadic el
   [[& phases] tag template phase-map]
   (with-target-template template
     (with-target-tag tag
-      (string/join
-       ""
-       (map (fn [phase]
-              (if (keyword? phase)
-                (output-resources phase phase-map)
-                (output-resources (first phase) (second phase))))
-            (phase-list phases))))))
+      (binding [*file-transfers* {}]
+        (string/join
+         ""
+         (map (fn [phase]
+                (if (keyword? phase)
+                  (output-resources phase phase-map)
+                  (output-resources (first phase) (second phase))))
+              (phase-list phases)))))))
 
 (defmacro build-resources
   "Outputs the resources specified in the body for the specified phases.

test/pallet/resource/remote_file_test.clj
@@ -18,7 +18,19 @@
   (is (= "wget -O file1 http://xx.com/abc\necho MD5 sum is $(md5sum file1)\nchown  user1 file1\n"
          (build-resources
           [] (remote-file "file1" :url "http://xx.com/abc" :owner "user1"))))
-  (is (= "if [ ! \\( -e file1 -a \\( \"abcd\" == \"$(md5sum file1 | cut -f1 -d' ')\" \\) \\) ]; then wget -O file1 http://xx.com/abc;fi\necho MD5 sum is $(md5sum file1)\nchown  user1 file1\n"
+  (is (= "if [ ! -e file1 ]; then\nif [ ! \\( \"abcd\" == \"$(md5sum file1 | cut -f1 -d' ')\" \\) ]; then wget -O file1 http://xx.com/abc;fi\nfi\necho MD5 sum is $(md5sum file1)\nchown  user1 file1\n"
          (build-resources
           [] (remote-file
-              "file1" :url "http://xx.com/abc" :md5 "abcd" :owner "user1")))))
+              "file1" :url "http://xx.com/abc" :md5 "abcd" :owner "user1"))))
+
+  (is (thrown-with-msg? RuntimeException
+        #".*/some/non-existing/file.*does not exist, is a directory, or is unreadable.*"
+        (build-resources
+         [] (remote-file
+             "file1" :local-file "/some/non-existing/file" :owner "user1"))))
+
+  (with-temporary [tmp (tmpfile)]
+    (is (re-find #"mv pallet-transfer-[a-f0-9-]+ file1"
+         (build-resources
+          [] (remote-file
+              "file1" :local-file (.getPath tmp)))))))

src/pallet/resource/remote_file.clj
@@ -19,9 +19,10 @@
         (cmd-join
           [(cond
              (and url md5) (script
-                             (if-not (&& (file-exists? ~path)
-                                       (== ~md5 @(md5sum ~path "|" cut "-f1 -d' '")))
-                               (wget "-O" ~path ~url))
+                             (if-not (file-exists? ~path)
+                               (do (if-not
+                                       (== ~md5 @(md5sum ~path "|" cut "-f1 -d' '"))
+                                     (wget "-O" ~path ~url))))
                              (echo "MD5 sum is" @(md5sum ~path)))
              url (script
                    (wget "-O" ~path ~url)

test/pallet/crate/hudson_test.clj
@@ -10,7 +10,7 @@
         [clojure.contrib.java-utils :only [file]]))
 
 (deftest hudson-tomcat-test
-  (is (= "mkdir -p /var/lib/hudson\nchown  root /var/lib/hudson\nchgrp  tomcat6 /var/lib/hudson\nchmod  775 /var/lib/hudson\nif [ ! \\( -e /var/lib/hudson/hudson.war -a \\( \"680e1525fca0562cfd19552b8d8174e2\" == \"$(md5sum /var/lib/hudson/hudson.war | cut -f1 -d' ')\" \\) \\) ]; then wget -O /var/lib/hudson/hudson.war http://hudson-ci.org/latest/hudson.war;fi\necho MD5 sum is $(md5sum /var/lib/hudson/hudson.war)\ncat > /etc/tomcat6/policy.d/99hudson.policy <<'EOF'\ngrant codeBase \"file:${catalina.base}/webapps/hudson/-\" {\n  permission java.security.AllPermission;\n};\ngrant codeBase \"file:/var/lib/hudson/-\" {\n  permission java.security.AllPermission;\n};\nEOF\ncat > /etc/tomcat6/Catalina/localhost/hudson.xml <<'EOF'\n<?xml version=\"1.0\" encoding=\"utf-8\"?>\n <Context\n privileged=\"true\"\n path=\"/hudson\"\n allowLinking=\"true\"\n swallowOutput=\"true\"\n >\n <Environment\n name=\"HUDSON_HOME\"\n value=\"/var/lib/hudson\"\n type=\"java.lang.String\"\n override=\"false\"/>\n </Context>\nEOF\ncp /var/lib/hudson/hudson.war /var/lib/tomcat6/webapps/hudson.war\ntouch  /var/lib/tomcat6/webapps/hudson.war\nchown  tomcat6 /var/lib/tomcat6/webapps/hudson.war\nchgrp  tomcat6 /var/lib/tomcat6/webapps/hudson.war\nchmod  600 /var/lib/tomcat6/webapps/hudson.war\n"
+  (is (= "mkdir -p /var/lib/hudson\nchown  root /var/lib/hudson\nchgrp  tomcat6 /var/lib/hudson\nchmod  775 /var/lib/hudson\nif [ ! -e /var/lib/hudson/hudson.war ]; then\nif [ ! \\( \"680e1525fca0562cfd19552b8d8174e2\" == \"$(md5sum /var/lib/hudson/hudson.war | cut -f1 -d' ')\" \\) ]; then wget -O /var/lib/hudson/hudson.war http://hudson-ci.org/latest/hudson.war;fi\nfi\necho MD5 sum is $(md5sum /var/lib/hudson/hudson.war)\ncat > /etc/tomcat6/policy.d/99hudson.policy <<'EOF'\ngrant codeBase \"file:${catalina.base}/webapps/hudson/-\" {\n  permission java.security.AllPermission;\n};\ngrant codeBase \"file:/var/lib/hudson/-\" {\n  permission java.security.AllPermission;\n};\nEOF\ncat > /etc/tomcat6/Catalina/localhost/hudson.xml <<'EOF'\n<?xml version=\"1.0\" encoding=\"utf-8\"?>\n <Context\n privileged=\"true\"\n path=\"/hudson\"\n allowLinking=\"true\"\n swallowOutput=\"true\"\n >\n <Environment\n name=\"HUDSON_HOME\"\n value=\"/var/lib/hudson\"\n type=\"java.lang.String\"\n override=\"false\"/>\n </Context>\nEOF\ncp /var/lib/hudson/hudson.war /var/lib/tomcat6/webapps/hudson.war\ntouch  /var/lib/tomcat6/webapps/hudson.war\nchown  tomcat6 /var/lib/tomcat6/webapps/hudson.war\nchgrp  tomcat6 /var/lib/tomcat6/webapps/hudson.war\nchmod  600 /var/lib/tomcat6/webapps/hudson.war\n"
          (build-resources [] (tomcat-deploy)))))
 
 (deftest determine-scm-type-test

pallet/git-crate

commit 854d8b4c20f4f6a7a1f5c7d5bc07f77b47c0b211

Fixed file transfers broken in 3bb22c7792005fa711c66c4b77d31fe18cb0ec69 and added a live test in remote-file using localhost

src/pallet/core.clj
@@ -24,7 +24,7 @@ tag as a configuration target.
   (:use
    [pallet.utils
     :only [remote-sudo make-user *admin-user* default-public-key-path
-           as-string]]
+           as-string *file-transfers*]]
    [pallet.resource
     :only [produce-phases defphases]]
    [pallet.compute
@@ -206,10 +206,11 @@ script that is run with root privileges immediatly after first boot."
         options (if port [:port port] [])]
     (if node-info
       (doseq [phase phases]
-        (when-let [script (produce-phases [phase] (node-info :tag) (node-info :image)
-                                          (node-info :phases))]
-          (info script)
-          (apply execute-script script node user options)))
+        (binding [*file-transfers* {}]
+          (when-let [script (produce-phases [phase] (node-info :tag) (node-info :image)
+                                            (node-info :phases))]
+            (info script)
+            (apply execute-script script node user options))))
       (error (str "Could not find node type for node " (tag node))))))
 
 (defn apply-phases

src/pallet/resource.clj
@@ -159,22 +159,22 @@ args is the argument signature for the resource, and must end with a variadic el
   [[& phases] tag template phase-map]
   (with-target-template template
     (with-target-tag tag
-      (binding [*file-transfers* {}]
-        (string/join
-         ""
-         (map (fn [phase]
-                (if (keyword? phase)
-                  (output-resources phase phase-map)
-                  (output-resources (first phase) (second phase))))
-              (phase-list phases)))))))
+      (string/join
+       ""
+       (map (fn [phase]
+              (if (keyword? phase)
+                (output-resources phase phase-map)
+                (output-resources (first phase) (second phase))))
+            (phase-list phases))))))
 
 (defmacro build-resources
   "Outputs the resources specified in the body for the specified phases.
    This is useful in testing."
   [[& phases] & body]
-  `(produce-phases
-    ~phases *target-tag* *target-template*
-    (resource-phases ~@body)))
+  `(binding [*file-transfers* {}]
+     (produce-phases
+      ~phases *target-tag* *target-template*
+      (resource-phases ~@body))))
 
 
 

src/pallet/utils.clj
@@ -180,15 +180,16 @@
               tmpfile (string/chomp (mktemp-result :out))
               channel (ssh-sftp session)]
           (assert (zero? (mktemp-result :exit)))
-          (sftp channel :put (java.io.ByteArrayInputStream.
-                              (.getBytes (str prolog command))) tmpfile)
-          (doseq [[file remote-name] *file-transfers*]
-            (info (format "Transferring file %s to node @ %s" file remote-name))
-            (sftp channel
-                  :put (-> file java.io.FileInputStream.
-                           java.io.BufferedInputStream.)
-                  remote-name)
-            (sftp channel :chmod 600 remote-name))
+          (with-connection channel
+            (sftp channel :put (java.io.ByteArrayInputStream.
+                                (.getBytes (str prolog command))) tmpfile)
+            (doseq [[file remote-name] *file-transfers*]
+              (info (format "Transferring file %s to node @ %s" file remote-name))
+              (sftp channel
+                    :put (-> file java.io.FileInputStream.
+                             java.io.BufferedInputStream.)
+                    remote-name)
+              (sftp channel :chmod 0600 remote-name)))
           (let [chmod-result (ssh session (str "chmod 755 " tmpfile) :return-map true)]
             (if (pos? (chmod-result :exit))
               (error (str "Couldn't chmod script : " ) (chmod-result :err))))

test/pallet/resource/remote_file_test.clj
@@ -1,9 +1,14 @@
 (ns pallet.resource.remote-file-test
   (:use [pallet.resource.remote-file] :reload-all)
   (:use [pallet.stevedore :only [script]]
-        [pallet.resource :only [build-resources]]
+        [pallet.resource :only [build-resources phase]]
         clojure.test
-        pallet.test-utils))
+        pallet.test-utils)
+  (:require
+   [pallet.core :as core]
+   [pallet.compute :as compute]
+   [pallet.utils :as utils]
+   [clojure.contrib.duck-streams :as io]))
 
 (deftest remote-file-test
   (is (= "cat > file1 <<EOF\nsomecontent\nEOF\n"
@@ -41,6 +46,19 @@
 
   (with-temporary [tmp (tmpfile)]
     (is (re-find #"mv pallet-transfer-[a-f0-9-]+ file1"
-         (build-resources
-          [] (remote-file
-              "file1" :local-file (.getPath tmp)))))))
+                 (build-resources
+                  [] (remote-file
+                      "file1" :local-file (.getPath tmp))))))
+
+  (with-temporary [tmp (tmpfile)
+                   target-tmp (tmpfile)]
+    (.delete target-tmp)
+    (io/copy "text" tmp)
+    (core/defnode tag [])
+    (core/apply-phases-to-node
+     nil (compute/make-unmanaged-node "tag" "localhost")
+     [(phase
+       (remote-file (.getPath target-tmp) :local-file (.getPath tmp)))]
+     utils/*admin-user*)
+    (is (.canRead target-tmp))
+    (is (= "text" (slurp (.getPath target-tmp))))))