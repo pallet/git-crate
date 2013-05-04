63920d8059f32113de6c6c1265c662b5d9fd13e2
Bruce Adams <bruce.adams@acm.org>
2013-05-04 14:50:23 -0400

    Fix mistaken "java" with "git"

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.palletops/git-crate "0.8.0-SNAPSHOT"
-  :description "Pallet crate to install, configure and use java"
+  :description "Pallet crate to install, configure and use git"
   :url "http://palletops.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :scm {:url "git@github.com:pallet/java-crate.git"}
+  :scm {:url "git@github.com:pallet/git-crate.git"}
 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.palletops/pallet "0.8.0-beta.1"]]
